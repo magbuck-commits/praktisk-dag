5405596eea413d1d068528165d6b1f1f53036a82
magbuck-commits <magbuck-commits@users.noreply.github.com>
2026-03-24 14:08:14 +0000

    deploy: 2a3673e29c0090bc92d08ae7ff7317257ae983c0

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'praktisk-dag-v2';
+const APP_SHELL = [
+  './',
+  './manifest.webmanifest',
+  './icons/app-icon.svg',
+  './html/index.html',
+  './html/day.html',
+  './css/styles.css',
+  './js/app.js',
+  './js/pwa.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const clone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return networkResponse;
+        })
+        .catch(() => caches.match('./html/index.html'));
+    })
+  );
+});
